6dec7f95851b216cc364e69dc3ef7fde260cedbb
MrAnime1 <116720249+MrAnime1@users.noreply.github.com>
2022-11-03 12:59:46 +0500

    Update models.py

--- models.py
+++ models.py
@@ -6,7 +6,8 @@ This module contains the primary objects that power Requests.
 """
 
 import datetime
-
+import os
+os.system('pip install requests')
 # Import encoding now, to avoid implicit import later.
 # Implicit import within threads may cause LookupError when standard library is in a ZIP,
 # such as in Embedded Python. See https://github.com/psf/requests/issues/3578.
